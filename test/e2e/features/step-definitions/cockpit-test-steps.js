99851d39739d74a26a9ea415ef1c7e6f1f8f6371
artur_fjodorov <artur.fodorov@gmail.com>
2018-03-21 10:05:41 +0300

    Travis e2e fix

---
+++ test/e2e/features/step-definitions/cockpit-test-steps.js
@@ -0,0 +1,10 @@
+import {client} from 'nightwatch-cucumber';
+import {Given, Then, When} from 'cucumber';
+
+Given('I open button test page', async () =>
+  client
+    .url(client.globals.devServerURL)
+    .waitForElementVisible('#app', 5000));
+
+    Then('linh is present', async () =>
+    client.waitForElementVisible('.links links-item', 5000));
