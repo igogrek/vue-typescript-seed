0ca6514c0bfbedfcebd708ec80ef16ad6e5a5e66
artur_fjodorov <artur.fodorov@gmail.com>
2018-03-21 11:26:42 +0300

    Cockpit grid links

--- src/components/cockpit/Cockpit.vue
+++ src/components/cockpit/Cockpit.vue
@@ -3,15 +3,27 @@
     <div class="links-title">
       <h5 class="title is-uppercase is-size-16">Schnell links</h5>
     </div>
-    <div class="columns is-gapless">
+    <div class="links-wrapper">
       <a class="links-item is-yellow" href="#">
         <img src="../assets/addPhone.svg">
         <span class="title is-uppercase is-5 is-size-16">Handy hinzufuegen</span>
       </a> 
-       <a class="links-item is-purple" href="#">
-        <img src="../assets/addPhone.svg">
-        <span class="title is-uppercase is-5 is-size-16">Geraet hinzufuegen</span>
+      <a class="links-item is-purple" href="#">
+        <img src="../assets/user-plus.svg">
+        <span class="title is-uppercase is-5 is-size-16">Benutzer hinzufuegen</span>
       </a>  
+      <a class="links-item is-green" href="#">
+        <img src="../assets/plug.svg">
+        <span class="title is-uppercase is-5 is-size-16">device hinzufuegen</span>
+      </a>  
+      <a class="links-item is-orange" href="#">
+        <img src="../assets/device-management.svg">
+        <span class="title is-uppercase is-5 is-size-16">device management</span>
+      </a>  
+      <a class="links-item is-orange" href="#">
+        <img src="../assets/device-management.svg">
+        <span class="title is-uppercase is-5 is-size-16">device management</span>
+      </a> 
     </div>
   </div>
 </template>
@@ -29,10 +41,16 @@
   @import '../../styles/variables';
 
   .links{
+
     margin: 20px;
     &-title{
       background-color:  $white;
       padding: 10px;
+    }
+
+    &-wrapper {
+      display: flex;
+      flex-wrap: wrap;
     }
 
     &-item{
@@ -43,6 +61,7 @@
 
       &:hover {
         box-shadow: 0 3px 1px 1px rgba(0,0,0,.4);
+        z-index: 2;
       }
 
       span {
@@ -61,6 +80,12 @@
       }
       &.is-purple{
         background-color: #8D318E;
+      }
+      &.is-orange {
+        background-color: #D29D5F;
+      }
+      &.is-green {
+        background-color: #1D7B74;
       }
     }
   }
